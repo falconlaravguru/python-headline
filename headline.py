f8c59dd124738eee4d43f28f0039842a312e40b0
falconlaravguru <falcon.laravguru@yandex.com>
2017-10-17 09:19:31 +1300

    Headline - BBC First Article Scrap

--- headline.py
+++ headline.py
@@ -1,9 +1,26 @@
+import feedparser
+
 from flask import Flask
 
 app = Flask(__name__)
 
+BBC_RSS = "http://feeds.bbci.co.uk/news/rss.xml"
+
 @app.route('/')
 def Index():
+    feed = feedparser.parse(BBC_RSS)
+    first_article = feed['entries'][0]
+
+    return """
+        <html>
+            <body>
+                <h1> BBC Headlines </h1>
+                <b> {0} </b> </br>
+                <b> {1} </b> </br>
+                <p> {2} </p> </br>
+            </body>
+        </html>
+    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
     return "Here's no news."
 
 if __name__ == '__main__':
