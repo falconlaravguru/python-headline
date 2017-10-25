0c161f3a762dd3e7454afc42a4b055e3030a439c
falconlaravguru <falcon.laravguru@yandex.com>
2017-10-26 06:00:24 +1300

    Get Input using request context from flask

--- headline.py
+++ headline.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 # Jinja template implemented
 from flask import render_template
+# Request context used
+from flask import request
 
 app = Flask(__name__)
 
@@ -15,8 +17,12 @@ RSS_FEEDS = {
 }
 
 @app.route('/')
-@app.route('/<publication>')
-def get_news(publication='bbc'):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS :
+        publication = "bbc"
+    else :
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
 
