e965407b42b738476dfd31174cace546c79faf17
falconlaravguru <falcon.laravguru@yandex.com>
2017-10-26 17:06:50 +1300

    Weather search form and python code modification

--- headline.py
+++ headline.py
@@ -20,35 +20,51 @@ RSS_FEEDS = {
     'iol' : 'http://www.iol.co.za/cmlink/1.640',
 }
 
-@app.route('/', methods=['GET','POST'])
-def get_news():
-    query = request.form.get("publication")
-    if not query or query.lower() not in RSS_FEEDS :
-        publication = "bbc"
-    else :
-        publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    location = "London, UK"
-    located_weather = get_weather(location)
+DEFAULTS = {
+    'publication':'bbc',
+    'city':'London,UK'
+}
 
-    return render_template("home.html", 
-                            articles = feed['entries'],
-                            weather = located_weather)
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=76cf4ebd7c79db726efc2ddeb6cd41b6"
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.form.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.form.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+
+    return render_template("home.html", articles=articles,
+                                        weather=weather)
+
+def get_news(publication):
+    if not publication or publication.lower() not in RSS_FEEDS :
+        publication = DEFAULTS["publication"]
+    else:
+        publication = publication.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+
+    return feed['entries']
 
 def get_weather(query):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=76cf4ebd7c79db726efc2ddeb6cd41b6"
+    
     query = urllib.quote(query)
-    url = api_url.format(query)
+    url = WEATHER_URL.format(query)
     data = urllib2.urlopen(url).read()
     parsed = json.loads(data)
     weather = None
-    if parsed.get("weather"):
+    if parsed.get('weather'):
         weather = {
-            "description": parsed['weather'][0]['description'],
-            "temperature": parsed['main']['temp'],
-            "city": parsed['name']
-        }
+                    'description':parsed['weather'][0]['description'],
+                    'temperature':parsed['main']['temp'],
+                    'city':parsed['name']
+                }
     return weather
 
 if __name__ == '__main__':
