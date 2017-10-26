15c77927796b267901ef731ca9b93cc52d53573a
falconlaravguru <falcon.laravguru@yandex.com>
2017-10-26 16:18:55 +1300

    Add the UK London Weather

--- headline.py
+++ headline.py
@@ -29,8 +29,27 @@ def get_news():
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
+    location = "London, UK"
+    located_weather = get_weather(location)
 
-    return render_template("home.html", articles = feed['entries'])
+    return render_template("home.html", 
+                            articles = feed['entries'],
+                            weather = located_weather)
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=76cf4ebd7c79db726efc2ddeb6cd41b6"
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {
+            "description": parsed['weather'][0]['description'],
+            "temperature": parsed['main']['temp'],
+            "city": parsed['name']
+        }
+    return weather
 
 if __name__ == '__main__':
     app.run(port = 5000, debug = True)
